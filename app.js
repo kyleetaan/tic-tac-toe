72618f100c0d130685df0abf15ed43addc2809dc
Kyle <tan.kyleemmanuel@gmail.com>
2022-02-16 09:53:05 +0800

    working winning condition

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const intro = document.querySelector(".intro");
 const startGame = document.getElementById("start-game");
-const board = document.getElementById("game-board");
+
 
 //change function to the one selecting a player
 startGame.addEventListener('click', () => {
@@ -10,14 +10,18 @@ startGame.addEventListener('click', () => {
 
 //module for game board
 const gameBoard = (function () {
-    const _boardArray = []
+    const _boardArray = [];
     
     function populateArray() {
         if(_boardArray.length === 0) {
             for(let i = 0; i < 9; i++){
-                _boardArray.push("_");
+                _boardArray.push("");
             }
         }
+    }
+
+    function updateArray(indexInArray, sign) {
+        _boardArray[indexInArray] = sign;
     }
 
     function getArray() {
@@ -27,39 +31,108 @@ const gameBoard = (function () {
     populateArray();
 
     return {
-        getArray : getArray
+        getArray : getArray,
+        updateArray: updateArray
     }
 })();
 
 
 //factory for player
-const player = () => {
-    const _xAttack = "X";
-    const _oAttack = "O";
+const player = (sign) => {
+    const _sign = sign;
 
+    function getSign() {
+        return _sign;
+    }
 
+    return {
+        getSign: getSign
+    }
 }
 
 //module for display
 const displayController = (() => {
-    let boardArr = gameBoard.getArray();
-    //do something
-    function populateBoard() {
-        for(let i = 0; i < boardArr.length; i++){
-            if(boardArr[i] !== "_"){
+    const boardArr = gameBoard.getArray();
+    const _board = document.getElementById("game-board");
+    const domArr = [..._board.children];
 
-            }
-        }
+    function updateBoard () {
+        domArr.forEach(element => {
+            element.textContent = boardArr[domArr.indexOf(element)];
+        })
+    }
+    
+    return {
+        updateBoard: updateBoard,
+
     }
 })();
 
+//module for game
+const gameController = (() => {
+    const playerOne = player("X");
+    const playerTwo = player("Y");
+    const _board = document.getElementById("game-board");
+    const domArr = [..._board.children];
+    let round = 0;
+
+    domArr.forEach(element => {
+        element.addEventListener('click', gameLoop)
+    })
+    
+    function gameLoop() {
+        if(round % 2 !== 0){
+            placePiece(playerTwo.getSign(), this);
+        }
+        else{
+            placePiece(playerOne.getSign(), this);
+        }
+        checkWin();
+    }
+
+    const winningConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    
+    function placePiece(sign, that){  
+        if(that.textContent === ""){
+            round++;
+            const index = domArr.indexOf(that);
+            gameBoard.updateArray(index, sign);
+            that.textContent = sign;
+        }
+    }
+    
+    function checkWin() {
+        const gameArray = gameBoard.getArray();
+        for(let i = 0; i < winningConditions.length; i++){       
+            if(gameArray[winningConditions[i][0]] === gameArray[winningConditions[i][1]] &&
+            gameArray[winningConditions[i][1]] === gameArray[winningConditions[i][2]] && 
+            gameArray[winningConditions[i][0]] !== ""){
+                // function here
+                break;
+            }
+        }
+    }
+    
+    return {
+        
+    }
+})();
 
 //working get index of div
-const test = document.getElementById("test");
-test.addEventListener('click', testFunction)
+// const test = document.getElementById("test");
+// test.addEventListener('click', testFunction)
 
-function testFunction() {
-    const index = [...board.children].indexOf(this);
-    console.log(index)
-}
+// function testFunction() {
+//     const index = [...board.children].indexOf(this);
+//     console.log(index)
+// }
 
